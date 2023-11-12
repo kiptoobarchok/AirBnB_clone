e381b945d5e5576f3e9813a12c5fe9a5f95c6e3d
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-11-13 01:46:45 +0300

    cmd

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    "command line interpreter"
+    prompt = '(hbnb) '
+
+    def do_EOF(self, line):
+        "Exit"
+        print("")
+        return True
+
+    def do_quit(self, line):
+        "exit command interpreter"
+        return True
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
