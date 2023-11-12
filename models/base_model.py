1cf630d3cb85e89865d3dd74a52303b448c34906
kiptoobrachok <calebkiptoo9090@gmail.com>
2023-11-13 01:25:07 +0300

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,29 @@ from datetime import datetime
 
 class BaseModel():
     "define base class"
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at  = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        "initialize instance attribute"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+
+                if key != '__class__':
+                    setattr(self, key, value)
+            
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+
+            if 'created_at' not in kwargs:
+                self.created_at  = datetime.now()
+            
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         "return a string representation"
