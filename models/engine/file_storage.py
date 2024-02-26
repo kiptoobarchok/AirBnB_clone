defa37b62a79e6a8526713cd2dbd3d7eb78cabef
kiptoobrachok <calebkiptoo9090@gmail.com>
2024-02-27 01:58:02 +0300

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ module to define class that manages file storage for hbnb
 """
 import json
 
+
 class FileStorage:
     """class to manage of hbnb models in JSON format"""
     __file_path = "file.json"
@@ -18,7 +19,7 @@ class FileStorage:
     def all(self):
         """returns dictionary of models in storage"""
         return FileStorage.__objects
-    
+
     def save(self):
         "save storage dictionary to file"
         with open(FileStorage.__file_path, 'w') as f:
@@ -28,4 +29,16 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
-    
+    def reload(self):
+        """
+        Deserialize the JSON file to __objects if it exists
+        """
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, 'r') as f:
+                data = json.load(f)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    module = __import__('models.' + class_name.lower(), fromlist=[class_name])
+                    class_ = getattr(module, class_name)
+                    instance = class_(**value)
+                    FileStorage.__objects[key] = instance
